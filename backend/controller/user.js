13e7d45cb169609c1cde5cb066410da1f667345b
Cao Minh Hiếu <22130081@st.hcmuaf.edu.vn>
2026-06-03 21:49:06 +0700

    Complete the contact function.

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -139,6 +139,9 @@ userController.addContact = async (req, res) => {
       [name, email, subject, message]
     );
 
+    // Send email to admin
+    await sendContactNotification({ name, email, subject, message });
+
     res.status(200).json({ success: true, message: "Your message has been sent! We will get back to you soon." });
   } catch (error) {
     console.error("Error saving contact:", error.message);
@@ -146,7 +149,7 @@ userController.addContact = async (req, res) => {
   }
 };
 
-const { transporter, sendOtpMail } = require("../utils/mail");
+const { transporter, sendOtpMail, sendContactNotification } = require("../utils/mail");
 
 userController.forgotPassword = async (req, res) => {
   const { email } = req.body;
